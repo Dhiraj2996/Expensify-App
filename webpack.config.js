d65ac090753f37cf74a8e19c193facca5826ba8b
Dhananjay Mundhada <dmundhada15@gmail.com>
2020-11-25 17:04:06 +0530

    Production error state

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,35 @@
 //entry->output
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  entry: "./src/app.js",
-  // entry: "./src/playground/redux-expensify.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  devtool: "eval-cheap-module-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    compress: true,
-    port: 9000,
-    historyApiFallback: true,
-  },
+module.exports = (env) => {
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
+  return {
+    entry: "./src/app.js",
+    // entry: "./src/playground/redux-expensify.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          //use: ["style-loader", "css-loader", "sass-loader"],
+          use: CSSExtract.extract(["css-loader", "sass-loader"]),
+        },
+      ],
+    },
+    plugins: [CSSExtract],
+    devtool: isProduction ? "source-map" : "eval-cheap-module-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+    },
+  };
 };
