7e8cef6f909d830c2e7c56627914e85a36db3a2c
Dhananjay Mundhada <dmundhada15@gmail.com>
2020-11-25 16:06:05 +0530

    Removed manual dispatches

---
+++ readme.md
@@ -0,0 +1,1 @@
+A expense tracker made using React
--- src/app.js
+++ src/app.js
@@ -11,23 +11,6 @@ import "./styles/styles.scss";
 
 const store = configureStore();
 
-store.dispatch(
-  addExpense({ description: "Rent", amount: 7000, createdAt: 1000 })
-);
-const coffeeExpense = store.dispatch(
-  addExpense({ description: "coffee", amount: 2000, createdAt: -1000 })
-);
-const teaExpense = store.dispatch(
-  addExpense({ description: "tea", amount: 1000 })
-);
-// console.log(
-//   getVisibleExpense(store.getState().expenses, store.getState().filters)
-// );
-// store.dispatch(setTextFilter("Cof"));
-// console.log(
-//   getVisibleExpense(store.getState().expenses, store.getState().filters)
-// );
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
