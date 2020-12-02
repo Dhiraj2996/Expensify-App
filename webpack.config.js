8c59ca67feb9dd3615a38491e861c109a772946d
Dhananjay Mundhada <dmundhada15@gmail.com>
2020-12-02 13:52:51 +0530

    setup heroku

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,7 @@ module.exports = (env) => {
     devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
       contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
     },
   };
 };
